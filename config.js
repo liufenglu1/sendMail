f187c1d30e0212ef9a03a0e4aff6e1b4117484f8
fengluliu <fengluliu@upchina.com>
2021-10-11 10:47:09 +0800

    首次提交

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '阿璐', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '664023452@qq.com', // 发送者邮箱
+  pass: 'inbanghcdptbbcag', // 发送者邮箱MTP协议密码
+  to: '875200714@qq.com', // 发送到谁，填邮箱
+  weatherKey: '4200e8f090484370b2ff5d4a1b3cf5eb', // 和风天气key
+  location: '101200113',  // 和风天气-武汉洪山区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: 'b995587e1a79f8fc53bce179b0710de1', // 天行数据的key
+  startDay: '2016-10-8', // 在一起的日期
+}
